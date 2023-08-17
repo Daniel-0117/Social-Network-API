bc922d1245c44be47af58c7ac170953e0b1e839e
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-17 09:28:07 -0700

    routes fix

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
+router.use('/posts', thoughtRoutes);
 
 module.exports = router;
